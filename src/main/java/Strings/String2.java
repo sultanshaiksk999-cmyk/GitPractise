c89b63e96c9c4fd80326d99d5a9d6c488ea637ae
shaik <sultanshaiksk999@gmail.com>
2025-11-15 09:09:22 +0530

    initial commit for Master

---
+++ src/main/java/Strings/String2.java
@@ -0,0 +1,10 @@
+package Strings;
+
+public class String2 {
+    public static void main(String[] args) {
+        String str1 = "abc";
+        String str2 = "abc";
+        System.out.println(str1 == str2);
+        System.out.println(str1.equals(str2));
+    }
+}
