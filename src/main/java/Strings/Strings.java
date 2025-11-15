bec1cd6ef1fd77b45dc6d995a5ce793ad24eb63d
shaik <sultanshaiksk999@gmail.com>
2025-11-15 08:52:18 +0530

    initial commit 2

---
+++ src/main/java/Strings/Strings.java
@@ -0,0 +1,9 @@
+package Strings;
+
+public class Strings {
+    public static void main(String[] args) {
+        String str1 = "abc";
+        String str2 = "abc";
+        System.out.println(str1 == str2);
+    }
+}
